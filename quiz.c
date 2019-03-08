ea3a9adc6534c75cc418318d9eac3a67e5b0ad91
ImagClaw <dal.whelpley@gmail.com>
2019-03-08 12:58:22 -0600

    uploaded quiz.c

---
+++ quiz.c
@@ -0,0 +1,22 @@
+/* QUIZ PROJECT 
+* 
+* char Ogden, Whelpley, Mejia;
+* 
+* Quiz Generator:
+*     - The quiz will ask user for a name input.
+*     - Ask the user how many questions >10 && < 100
+*     - Questions will be randomly queried from a predefined list
+*     - Users receive 1 point per correct answer
+*     - Score will be represented at the end as a percentage
+* 
+*/
+
+
+//#incuded header for program functionality
+#include <stdio.h>
+
+
+int main() {
+    
+    return 0;
+}
