3ec9b050b4bc5da38bcbb55a1f3f8a88923bdab8
ImagClaw <dal.whelpley@gmail.com>
2019-03-09 00:40:36 -0600

    Added simple menu

--- quiz.c
+++ quiz.c
@@ -14,9 +14,31 @@
 
 //#incuded header for program functionality
 #include <stdio.h>
+//#include "ogden.h"
 
 
 int main() {
-    
+    // Variables
+    char name[64];
+    int numQ;
+
+    // Ask user for name
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("\n\nHello there, %s.\n", name);
+    printf("How many questions would you like to be tested on? \n");
+    do {
+        printf("Enter question amount here (10-20): ");
+        scanf("%d", &numQ);
+        if (numQ <= 9) {
+            printf("**ERROR: Question amount must be more than 10.\n\n");
+        } else if (numQ >= 21) {
+            printf("**ERROR: Question amount must be less than 20.\n\n");
+        } else {
+            printf("You've entered %d questions.\n\n", numQ);
+        }
+    } while ((numQ <= 9) || (numQ >=21));
+
     return 0;
 }
