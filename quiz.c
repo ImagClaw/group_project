15588eee2742448fc84c233a41bbe239b7edaa2a
ImagClaw <dal.whelpley@gmail.com>
2019-03-12 13:23:09 -0500

    updated

--- quiz.c
+++ quiz.c
@@ -60,18 +60,6 @@ char getche(void)
 {
   return getch_(1);
 }
-
-/* Let's test it out */
-int main(void) {
-  char c;
-  printf("(getche example) please type a letter: ");
-  c = getche();
-  printf("\nYou typed: %c\n", c);
-  printf("(getch example) please type a letter...");
-  c = getch();
-  printf("\nYou typed: %c\n", c);
-  return 0;
-} 
 #endif
 
 //#incuded header for program functionality
