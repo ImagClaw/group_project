2e8e61de521c16a910479f5aa220c27bb3559a89
ImagClaw <dal.whelpley@gmail.com>
2019-03-15 12:23:31 -0500

    MOAR UPDOOTS

--- quiz.c
+++ quiz.c
@@ -48,13 +48,6 @@ void help() {
 
 void highScores() {
 
-    //struct highscore {
-    //    char name[64];
-    //    float score;
-    //};
-//
-    //struct highscore values[15], temp;
-
     char s[64];
     float n;
     int i = 0, lncount = 0;
